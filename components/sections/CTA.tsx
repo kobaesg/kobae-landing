8322f70e1825b220134fa67b93f959bbdba8f1d1
kobaepilot <kobaepilot@gmail.com>
2026-02-18 10:24:50 +0800

    Update CTA.tsx

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -15,13 +15,13 @@ export function CTA() {
                     className="text-center flex flex-col items-center gap-12"
                 >
                     <h2 className="text-5xl md:text-6xl lg:text-[68px] font-normal leading-tight lg:leading-[71px] tracking-tight">
-                        Kobae, the social app for shared circles.
+                        Kobae, the place for shared circles.
                     </h2>
                     <Button
                         size="lg"
                         className="bg-primary hover:bg-primary/90 text-white font-semibold text-lg rounded-[32px] h-16 px-8 shadow-[0px_0px_10px_0px_rgba(255,144,97,0.8)]"
                     >
-                        Sign Up Now
+                        Waitlist
                     </Button>
                 </motion.div>
             </div>
